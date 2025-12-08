ff49aa2738d893fe98c69db31d41f0d7eeae89a7
BG-Eddy <burgoseddy64@gmail.com>
2025-12-07 19:57:02 -0500

    Configurar frontend para Vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-// Creamos una instancia única de Axios
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
-  auth: {
-    username: "admin",
-    password: "admin123"
-  },
+  baseURL: BASE_URL,
+
   headers: {
     "Content-Type": "application/json",
   },
